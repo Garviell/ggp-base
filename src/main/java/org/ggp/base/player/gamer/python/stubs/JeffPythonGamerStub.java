40c514ee06fa369d7dea5308ce391a25953b0808
Garviell <faustusxi@gmail.com>
2016-02-02 18:33:49 +0000

    tiny base

---
+++ src/main/java/org/ggp/base/player/gamer/python/stubs/JeffPythonGamerStub.java
@@ -0,0 +1,12 @@
+package org.ggp.base.player.gamer.python.stubs;
+import org.ggp.base.player.gamer.python.PythonGamer;
+
+
+public final class JeffPythonGamerStub extends PythonGamer
+{
+    @Override
+    protected String getPythonGamerModule() { return "jeff_gamer"; }
+    @Override
+    protected String getPythonGamerName() { return "Jeff"; }
+}
+
